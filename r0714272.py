fb083babd09bd13619b9bc56fde2e47aa3018981
Florian Ramakers <florianramakers3@gmail.com>
2020-11-25 19:20:03 +0100

    created base structure for evolutionary algorithm

--- r0714272.py
+++ r0714272.py
@@ -1,5 +1,6 @@
 import Reporter
 import numpy as np
+import random as rnd
 
 class r0714272:
 
@@ -16,12 +17,16 @@ class r0714272:
 
 		ap = AlgorithmParameters(la=100, mu=50, init_alpha=0.05, init_beta=0.9)
 		tsp = TSP(distance_matrix, ap)
-		while not tsp.has_converged():
 
+		# Initialize the population
+		tsp.initialize()
+		while not tsp.has_converged():
 			(mean_obj, best_obj, best_sol) = tsp.report_values()
 			time_left = self.reporter.report(mean_obj, best_obj, best_sol)
 			if time_left < 0:
 				break
+
+			tsp.update()
 
 
 		return 0
@@ -38,7 +43,66 @@ class TSP:
 		self.distance_matrix = distance_matrix
 		self.params = params
 		self.n = distance_matrix.shape[0]
-		self.population = [Individual(np.random.permutation(self.n), params.init_alpha, params.init_beta)]
+		self.population = []
+		self.offsprings = []
+
+	def initialize(self):
+		"""
+		Initialize the population using random permutations and the initial values specified in the AlgorithmParameters object.
+		"""
+		self.population = [Individual(np.random.permutation(self.n), self.params.init_alpha, self.params.init_beta)]
+
+
+
+	def select(self):
+		"""
+		Select a parent from the population for recombination.
+		#TODO: implement a selection operator.
+		:return: An Individual object that represents a parent.
+		"""
+		pass
+
+	def create_offsprings(self):
+		""" Select 2 * mu parents from the population and apply a recombination operator on them. """
+		parents = [(self.select(), self.select()) for _ in range(self.params.mu)]
+		offsprings = []
+		for (p1,p2) in parents:
+			if rnd.random() <= p1.beta:
+				offsprings.append(self.recombine(p1,p2))
+			if rnd.random() <= p2.beta:
+				offsprings.append(self.recombine(p2,p1))
+
+	def recombine(self, parent1, parent2):
+		"""
+		#TODO: Implement a recombination operator.
+		:param parent1: The first parent.
+		:param parent2: The second parent.
+		:return: An Individual object that represents the offspring of parent1 and parent2
+		"""
+		pass
+
+	def mutate(self):
+		"""
+		Mutate the population.
+		#TODO: should the whole population be mutated?
+		"""
+		for ind in self.population:
+			ind.mutate()
+
+	def elimination(self):
+		"""
+		Eliminate certain Individuals from the population.
+		#TODO: implement an elimination operator
+		#TODO: implement a diversity check
+		"""
+		pass
+
+	def local_search(self):
+		"""
+		Apply local search to optimize the population.
+		#TODO: implement a local search operator
+		"""
+		pass
 
 	def has_converged(self):
 		"""
@@ -58,6 +122,14 @@ class TSP:
 		"""
 		return 0,0,0
 
+	"""
+	Update the population.
+	"""
+	def update(self):
+		self.create_offsprings()
+		self.mutate()
+		self.elimination()
+
 class Individual:
 	"""
 	A class that represents an order in which to visit the cities.
@@ -74,6 +146,19 @@ class Individual:
 		self.perm = perm
 		self.alpha = alpha
 		self.beta = beta
+
+	def mutate(self):
+		"""
+		Mutate this individual.
+		"""
+		if rnd.random() <= self.alpha:
+			self.mutate0()
+
+	def mutate0(self):
+		"""
+		#TODO: implement a mutation operator
+		"""
+		pass
 
 
 class AlgorithmParameters:
