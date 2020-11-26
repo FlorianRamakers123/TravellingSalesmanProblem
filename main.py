49b5e3e5e8411be02206575095bc70bc149f1d03
Florian Ramakers <florianramakers3@gmail.com>
2020-11-26 11:18:51 +0100

    First working version

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import matplotlib.pyplot as plt
 
 from r0714272 import r0714272
 
@@ -9,3 +10,20 @@ if __name__ == "__main__":
 
     filename = "{}.csv".format(sys.argv[1])
     r0714272().optimize(filename)
+
+    csv_content = [line.split(',') for line in open("r0714272.csv").read().split("\n")[2:] if line.strip() != ""]
+    iterations = [int(elems[0]) for elems in csv_content]
+    # elapsed_time = [float(elems[1]) for elems in csv_content]
+    mean_values = [float(elems[2]) for elems in csv_content]
+    best_values = [float(elems[3]) for elems in csv_content]
+
+    print("Last best value: {}".format(best_values[-1]))
+    print("Actual best value: {}".format(min(best_values)))
+
+    plt.plot(iterations, mean_values, label="Mean value")
+    plt.plot(iterations, best_values, label="Best value")
+    plt.xticks(iterations)
+    plt.legend()
+
+    plt.show()
+
--- r0714272.py
+++ r0714272.py
@@ -15,11 +15,12 @@ class r0714272:
 		distance_matrix = np.loadtxt(file, delimiter=",")
 		file.close()
 
-		ap = AlgorithmParameters(la=100, mu=50, init_alpha=0.05, init_beta=0.9)
+		ap = AlgorithmParameters(la=100, mu=50, init_alpha=0.05, init_beta=0.9, k=3, max_iter=500)
 		tsp = TSP(distance_matrix, ap)
 
 		# Initialize the population
 		tsp.initialize()
+
 		while not tsp.has_converged():
 			(mean_obj, best_obj, best_sol) = tsp.report_values()
 			time_left = self.reporter.report(mean_obj, best_obj, best_sol)
@@ -27,7 +28,6 @@ class r0714272:
 				break
 
 			tsp.update()
-
 
 		return 0
 
@@ -38,13 +38,23 @@ class TSP:
 		"""
 		Create a new TSPAlgorithm object.
 		:param distance_matrix: The distance matrix that contains the distances between all the cities.
-		:param params: A AlgorithmParameters object that contains all the parameter values for executing the algorithm
+		:param params: A AlgorithmParameters object that contains all the parameter values for executing the algorithm.
 		"""
 		self.distance_matrix = distance_matrix
 		self.params = params
 		self.n = distance_matrix.shape[0]
 		self.population = []
 		self.offsprings = []
+		self.iterations = 0
+
+	def fitness(self, ind):
+		"""
+		Calculate the fitness value of the given individual.
+		:param ind: The Individual object to calculate the fitness for.
+		:return: The fitness value of the given individual.
+		"""
+		length = ind.perm.shape[0]
+		return sum(self.distance_matrix[int(ind.perm[i % length]), int(ind.perm[(i + 1) % length])] for i in range(length))
 
 	def initialize(self):
 		"""
@@ -52,34 +62,50 @@ class TSP:
 		"""
 		self.population = [Individual(np.random.permutation(self.n), self.params.init_alpha, self.params.init_beta)]
 
-
-
 	def select(self):
 		"""
 		Select a parent from the population for recombination.
-		#TODO: implement a selection operator.
+		### CURRENT IMPLEMENATION K-tournament
 		:return: An Individual object that represents a parent.
 		"""
-		pass
+		selected = rnd.choices(self.population, k=self.params.k)
+		selected = sorted(selected, key=lambda ind: self.fitness(ind)) 	# TODO: sorting using the fitness value might be to computationally expensive
+		return selected[0]
 
 	def create_offsprings(self):
 		""" Select 2 * mu parents from the population and apply a recombination operator on them. """
 		parents = [(self.select(), self.select()) for _ in range(self.params.mu)]
-		offsprings = []
 		for (p1,p2) in parents:
 			if rnd.random() <= p1.beta:
-				offsprings.append(self.recombine(p1,p2))
+				self.offsprings.append(self.recombine(p1,p2))
 			if rnd.random() <= p2.beta:
-				offsprings.append(self.recombine(p2,p1))
+				self.offsprings.append(self.recombine(p2,p1))
 
 	def recombine(self, parent1, parent2):
 		"""
-		#TODO: Implement a recombination operator.
+		### CURRENT IMPLEMENTATION: order crossover
 		:param parent1: The first parent.
 		:param parent2: The second parent.
 		:return: An Individual object that represents the offspring of parent1 and parent2
 		"""
-		pass
+		length = parent1.perm.shape[0]
+		(start, end) = random_ind(length)
+
+		offspring_perm = np.ones(length).astype(int)
+		offspring_perm[start:end + 1] = parent1.perm[start:end + 1]
+
+		k = 0
+
+		for i in range(length):
+			if parent2.perm[(end + i + 1) % length] not in offspring_perm:
+				offspring_perm[(end + k + 1) % length] = parent2.perm[(end + i + 1) % length]
+				k += + 1
+
+		c1 = 2 * (rnd.random() - 0.5)
+		new_alpha = parent1.alpha + c1 * (parent2.alpha - parent1.alpha)
+		c2 = 2 * (rnd.random() - 0.5)
+		new_beta = parent1.beta + c2 * (parent2.beta - parent1.beta) #TODO: should beta be self-adapted?
+		return Individual(offspring_perm, new_alpha, new_beta)
 
 	def mutate(self):
 		"""
@@ -92,10 +118,11 @@ class TSP:
 	def elimination(self):
 		"""
 		Eliminate certain Individuals from the population.
-		#TODO: implement an elimination operator
+		### CURRENT IMPLEMENTATION: lambda + mu elimination
 		#TODO: implement a diversity check
 		"""
-		pass
+		#TODO: maybe we can keep the population sorted and merge the offsprings?
+		self.population = sorted(self.offsprings + self.population, key= lambda ind: self.fitness(ind))[0:self.params.la]
 
 	def local_search(self):
 		"""
@@ -107,9 +134,10 @@ class TSP:
 	def has_converged(self):
 		"""
 		Check whether the algorithm has converged and should be stopped
+		### CURRENT IMPLEMENTATION: iteration count
 		:return: True if the algorithm should stop, False otherwise
 		"""
-		return True
+		return self.iterations >= self.params.max_iter
 
 	def report_values(self):
 		"""
@@ -120,7 +148,17 @@ class TSP:
 			  with city numbering starting from 0
 		:return: A tuple (m, bo, bs) that represent the mean objective, best objective and best solution respectively
 		"""
-		return 0,0,0
+		mean = 0
+		best_fitness = -1
+		best_individual = None
+		for ind in self.population:
+			f = self.fitness(ind)
+			mean += f
+			if f < best_fitness or best_fitness == -1:
+				best_fitness = f
+				best_individual = ind
+		mean = mean / len(self.population)
+		return mean, best_fitness, best_individual.perm
 
 	"""
 	Update the population.
@@ -128,7 +166,9 @@ class TSP:
 	def update(self):
 		self.create_offsprings()
 		self.mutate()
+		self.local_search()
 		self.elimination()
+		self.iterations += 1
 
 class Individual:
 	"""
@@ -156,9 +196,10 @@ class Individual:
 
 	def mutate0(self):
 		"""
-		#TODO: implement a mutation operator
+		### CURRENT IMPLEMENTATION: inversion mutation
 		"""
-		pass
+		(start, end) = random_ind(self.perm.shape[0])
+		self.perm[start:end] = np.flip(self.perm[start:end])
 
 
 class AlgorithmParameters:
@@ -169,12 +210,31 @@ class AlgorithmParameters:
 		* mu: The amount of tries to create offsprings (not every try will result in offsprings)
 		* init_alpha: The initial vlaue for alpha (the probability of mutation)
 		* init_beta: The initial value for beta (the probability of recombination)
+		### THESE MIGHT CHANGE
+		* k: the parameter for the k-tournament selection
+		* max_iter: the maximum amount of iterations the algorithm can run
+
 	"""
 
-	def __init__(self, la, mu, init_alpha, init_beta):
+	def __init__(self, la, mu, init_alpha, init_beta, k, max_iter):
 		self.la = la
 		self.mu = mu
 		self.init_alpha = init_alpha
 		self.init_beta = init_beta
+		self.k = k
+		self.max_iter = max_iter
 
 
+### UTILITY METHODS
+
+def random_ind(n):
+	"""
+	Generate two random numbers between 0 (inclusive) and n (exclusive)
+	:param n: The exclusive upperbound
+	:return: a tuple (i1,i2) where 0 <= i1,i2 < n and i1 != i2
+	"""
+	i1 = rnd.randrange(0, n)
+	i2 = rnd.randrange(0, n)
+	while i1 == i2:
+		i2 = rnd.randrange(0, n)
+	return min(i1, i2), max(i1, i2)
